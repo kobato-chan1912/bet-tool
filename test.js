979174bc7d448149d06683e66a2d9610930e1d06
kobato-chan1912 <no_body156@yahoo.com.vn>
2025-03-19 22:31:25 +0700

    update testJS

--- test.js
+++ test.js
@@ -2,43 +2,58 @@ const puppeteer = require('puppeteer');
 const os = require("os")
 const path = require("path")
 // Or import puppeteer from 'puppeteer-core';
+const pLimit = require('p-limit');
+const limitThreads = 100
+let limit = pLimit(limitThreads);
 
 async function main() {
-    // Launch the browser and open a new blank page
-    let browserOptions = {
-        headless: false
-      };
-    
-      if (os.platform() === 'win32') {
-        browserOptions.executablePath = path.join('chrome', 'chrome.exe');
-      }
-const browser = await puppeteer.launch(browserOptions);
-const page = await browser.newPage();
+  // Launch the browser and open a new blank page
+  let browserOptions = {
+    headless: false
+  };
+
+  if (os.platform() === 'win32') {
+    browserOptions.executablePath = path.join('chrome', 'chrome.exe');
+  }
+  const browser = await puppeteer.launch(browserOptions);
+  const page = await browser.newPage();
 
 
 
-// Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+  // Navigate the page to a URL.
+  await page.goto('https://developer.chrome.com/');
 
-// Set screen size.
-await page.setViewport({width: 1080, height: 1024});
+  // Set screen size.
+  await page.setViewport({ width: 1080, height: 1024 });
 
-// Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');
+  // Type into search box.
+  await page.locator('.devsite-search-field').fill('automate beyond recorder');
 
-// Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
+  // Wait and click on first result.
+  await page.locator('.devsite-result-item-link').click();
 
-// Locate the full title with a unique string.
-const textSelector = await page
-  .locator('text/Customize and automate')
-  .waitHandle();
-const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  // Locate the full title with a unique string.
+  const textSelector = await page
+    .locator('text/Customize and automate')
+    .waitHandle();
+  const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
-// Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);
+  // Print the full title.
+  console.log('The title of this blog post is "%s".', fullTitle);
 
-await browser.close();
+  await browser.close();
 }
 
-main()
+async function runMain() {
+  const tasks = [];
+  for (let i = 0; i < 105; i++)
+  {
+    tasks.push(limit(() => main())); 
+  }
+
+
+  await Promise.all(tasks);
+
+}
+
+runMain()
